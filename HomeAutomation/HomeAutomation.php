1ba493aa2fdcc18229965a89a69194ea220f7754
LordKain001 <35432416+LordKain001@users.noreply.github.com>
2018-12-20 21:44:51 +0100

    added Temperature sensor

--- HomeAutomation/HomeAutomation.php
+++ HomeAutomation/HomeAutomation.php
@@ -43,6 +43,9 @@ $db = new homeControlDatabase();
 $switchCommands = array();
 array_push($switchCommands, array("Adresse"=>"01","Port"=>0,"value"=>0));
 
+$w1Sensors = scandir("/sys/bus/w1/devices");
+$w1Sensors = array_filter($w1Sensors, "isW1Sensor");
+
 while(1)
 {
 	usleep(10000);
@@ -72,7 +75,66 @@ while(1)
 	}
 	
 
+
+	$temps = GetAllW1SensorTemps($w1Sensors);
+	if ($temps["28-041662c477ff"]["Temp"] >= 25.0) 
+	{
+		if ($switchCommands[0]["value"] != 0)
+		{
+			echo "not Heating \r";
+			$switchCommands[0]["value"] = 0;
+			$db->SetSwitchCommand($switchCommands);
+		}
+		
+	}
+	if ($temps["28-041662c477ff"]["Temp"] <= 22.0) 
+	{
+		if ($switchCommands[0]["value"] != 1)
+		{
+			echo "Heating \r";
+			$switchCommands[0]["value"] = 1;
+			$db->SetSwitchCommand($switchCommands);
+		}
+		
+	}
+	
+
 }
+
+
+
+
+function GetAllW1SensorTemps($w1Sensors)
+{
+	
+	$temperatures = array();	
+
+
+	foreach ($w1Sensors as $key) 
+	{
+		$path = "/sys/bus/w1/devices/" . $key . "/w1_slave";
+		$sensorTemp = file_get_contents($path);
+		$sensorTemp = substr($sensorTemp, strpos($sensorTemp, "t=") + 2);    
+		$temperatures[$key]["Temp"] = $sensorTemp/ 1000;
+		$temperatures[$key]["Time"] = time();
+	}
+	return $temperatures;
+}
+
+
+
+function isW1Sensor($value)
+{
+	$query = "28-";
+	if(substr( $value, 0, strlen($query) ) === $query)
+	{
+		return true;
+	}
+}
+
+
+
+
 
 
 ?>
